26500a40cae58b83d3a10ad69ca541856c7ce70c
Clóvis Magenta da Cunha <clovismagenta@gmail.com>
2019-01-14 18:10:43 -0200

    Initial Commit

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Clovis Magenta da Cunha on 14/01/19.
+//  Copyright © 2019 CMC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
